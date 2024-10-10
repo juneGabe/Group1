0f08f4cfd509310c66a6e77b6a4940bda8a33302
Rohit Kumar Singh <rohity294@gmail.com>
2024-10-09 20:19:55 -0400

    added optimized version

--- MyProjectWorkspace/ProblemSolvingDSACollections/src/main/java/mypack/TwoSum.java
+++ MyProjectWorkspace/ProblemSolvingDSACollections/src/main/java/mypack/TwoSum.java
@@ -1,5 +1,7 @@
 package mypack;
 
+import java.util.*;
+
 public class TwoSum {
 	public static void main(String[] args) {
         int[] nums = {10, 20, 5, 0, -1, 30};
@@ -10,10 +12,26 @@ public class TwoSum {
         System.out.println("Two numbers who add upto "+ targetSum + " are " + nums[result[0]] +", " + nums[result[1]] + ",and are present at indices" + result[0] +"," + result[1]);
         
         int[] result2 = getPairOptimized(nums, targetSum);//optimized
+        System.out.println("The answer indices are: " + result2[0] + "," + result2[1]);
 	}
 
 	private static int[] getPairOptimized(int[] nums, int targetSum) { //optimized
-		return null;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>(); //Key = Number In the Array , Value=Index where it occured in the array
+		int[] results = {-1, -1};
+		
+		for(int i=0; i<nums.length; i++) {//10:0 , 20:1, 5:2, 0:3, -1:4, 
+		   int num = nums[i];	//30
+		   int compliment = targetSum - num;//20
+		   if(map.containsKey(compliment)) {
+			   results[0] = i;
+			   results[1] = map.get(compliment);
+			   return results;
+		   }else {
+			   map.put(num, i);
+		   }
+		}
+		
+		return results;
 	}
 
 	private static int[] getPair(int[] nums, int targetSum) {//brute-force
